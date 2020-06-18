28ed3f31b17e1f79ebb3d60c27d13516eac3fd55
Jason Welsh <jasonbwelsh@gmail.com>
2020-06-18 16:35:53 -0400

    UI complete except top rght triangle

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,8 +5,11 @@ function Card() {
   return (
     <StyledCard>
       <div className="step-indicator__container">1</div>
-      <h1 className="card-heading">Heading</h1>
-      <p className="card-text">card text blah blah blah blah something</p>
+      <div className="content-container">
+        <h1 className="card-heading">Heading</h1>
+        <p className="card-text">card text blah blah blah blah something</p>
+      </div>
+
       <button className="card-btn">OK</button>
     </StyledCard>
   );
--- src/components/Card/StyledCard.js
+++ src/components/Card/StyledCard.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const StyledCard = styled.div`
-  height: 300px;
   width: 300px;
   border-radius: 5px;
   background: #fff;
@@ -9,19 +8,50 @@ export const StyledCard = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  align-items: center;
+  color: #8391a1;
+  box-shadow: 4px 8px 12px 0px rgba(0, 0, 0, 0.1);
 
   @media (min-width: 768px) {
-    height: 400px;
     width: 400px;
   }
 
-  // step indicicator
+  /* step indicicator */
   .step-indicator__container {
-    /* display: inline-block; */
+    height: 10px;
+    width: 10px;
     position: absolute;
     top: 0%;
     right: 0%;
     padding: 1rem;
-    background: grey;
+    background: #8391a1;
+    color: #fff;
+    /* border-radius: 50%; */
+    /* border-radius: 0 5px 0 100%; */
+    transform: rotate(45deg);
+  }
+
+  /* card-content */
+  .content-container {
+    width: 80%;
+
+    .card-heading {
+      color: #616e7d;
+    }
+
+    .card-text {
+      margin-bottom: 2rem;
+    }
+  }
+
+  /* bottom button */
+  .card-btn {
+    width: 100%;
+    border: none;
+    cursor: pointer;
+    padding: 1rem;
+    background: #046bde;
+    color: #fff;
+    border-radius: 0 0 5px 5px;
   }
 `;
